074ab7aef918fe19c28e961d9a5c98ea676f71b8
MoHiTopdEv <76580977+MoHiTopdEv@users.noreply.github.com>
2021-01-06 12:24:51 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
+  await client.user.setActivity("WITH 1K+ ONLINE MEMBERS IN BT", {
     type: "STREAMING", url: "https://www.twitch.tv/something" 
     //can be LISTENING, WATCHING, PLAYING, STREAMING
   });
